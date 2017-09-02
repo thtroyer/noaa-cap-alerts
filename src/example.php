2b5dcaf01bfed42ec3f3c0bc17f45b830e026d96
Tom Troyer <tom.troyer@gmail.com>
2017-09-02 01:42:28 -0400

    Reworked example

--- src/example.php
+++ src/example.php
@@ -3,6 +3,19 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 ini_set('default_socket_timeout', 120);
 
-$example = new NoaaCapParser\Example();
-$example->run();
+// Get the file from noaa
+$options = array(
+    'http' => array(
+        'header' => "User-agent: noaa-cap-parser',"
+    ),
+);
 
+$context = stream_context_create($options);
+$xml = file_get_contents('http://alerts.weather.gov/cap/us.php?x=0', false, $context);
+
+// Parse the file
+$noaaCapParser = new NoaaCapParser\NoaaCapParser();
+$alertArray = $noaaCapParser->parseFromXml($xml);
+
+// Show output
+print_r($alertArray);
