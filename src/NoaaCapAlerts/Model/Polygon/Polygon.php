8204289767aa2f39b6a8c0a91bfe828c582ebe43
Tom Troyer <tom.troyer@gmail.com>
2020-02-08 22:37:58 -0500

    Implementing isPointInPolygon()

--- src/NoaaCapAlerts/Model/Polygon/Polygon.php
+++ src/NoaaCapAlerts/Model/Polygon/Polygon.php
@@ -19,9 +19,64 @@ class Polygon
         $this->points = $points;
     }
 
-    function isPointInPolygon(Point $point) {
-        //@todo
-        throw new \Exception("Method not implemented.");
+    public function isPointInPolygon(Point $targetPoint): bool {
+        $targetLatitude = $targetPoint->getLatitude();
+        $longitudeHits = [];
+        $lastPoint = null;
+        foreach ($this->points as $point) {
+            if ($lastPoint == null) {
+                $lastPoint = $point;
+                continue;
+            }
+            if ($this->doLinesCrossLatitude($lastPoint, $point, $targetLatitude)) {
+                $longitudeHits[] = $this->findInterceptLongitude($lastPoint, $point, $targetLatitude);
+            }
+            $lastPoint = $point;
+        }
+
+        $longitudeHits = array_unique($longitudeHits);
+        sort($longitudeHits);
+
+        $hitsBefore = 0;
+        $hitsAfter = 0;
+
+        foreach($longitudeHits as $hit) {
+            if ($hit <= $targetPoint->getLongitude()) {
+                $hitsBefore++;
+            } elseif ($hit >= $targetPoint->getLongitude()) {
+                $hitsAfter++;
+            }
+        }
+
+        if (($hitsBefore % 2 == 1) && ($hitsAfter % 2 == 1)) {
+            return true;
+        } elseif (($hitsBefore % 2 == 0) && ($hitsAfter % 2 == 0)) {
+            return false;
+        }
+
+        throw new \Exception("Something weird happened.  Fixme");
+    }
+
+    private function doLinesCrossLatitude(Point $point1, Point $point2, float $latitude): bool {
+        if ($point1->getLatitude() >= $latitude && $point2->getLatitude() <= $latitude) {
+            return true;
+        } elseif ($point2->getLatitude() >= $latitude && $point1->getLatitude() <= $latitude) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function findInterceptLongitude(Point $point1, Point $point2, float $latitude): float
+    {
+        if ($point2->getLongitude() - $point1->getLongitude() == 0) {
+            return $point2->getLongitude();
+        }
+
+        $slope = ($point2->getLatitude() - $point1->getLatitude()) / ($point2->getLongitude() - $point1->getLongitude());
+        $b = $point1->getLatitude() - ($slope*$point1->getLongitude());
+
+        return ($latitude + $b) / $slope;
     }
 
     function __toString()
---
+++ tests/NoaaCapAlerts/Model/Polygon/PolygonTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace NoaaCapAlerts\Model\Polygon;
+
+class PolygonTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testPointInSimplePolygon()
+    {
+        $point = new Point(2.5, 2.5);
+
+        $polygon = new Polygon(
+            array(
+                new Point(0, 0),
+                new Point(5, 0),
+                new Point(5, 5),
+                new Point(0, 5),
+                new Point(0, 0),
+            )
+        );
+
+        $this->assertTrue($polygon->isPointInPolygon($point));
+    }
+
+    public function testPointNotInSimplePolygon()
+    {
+        $point = new Point(6, 2.5);
+
+        $polygon = new Polygon(
+            array(
+                new Point(0, 0),
+                new Point(5, 0),
+                new Point(5, 5),
+                new Point(0, 5),
+                new Point(0, 0),
+            )
+        );
+
+        $this->assertFalse($polygon->isPointInPolygon($point));
+    }
+}
